cbbaa3bd392898d09de6b09f6dd6297a8bc10808
Pushistost <110733035+Pushistost@users.noreply.github.com>
2025-07-26 13:19:05 +0300

    fix: re-export sqlite models and mixins

--- sqlite/__init__.py
+++ sqlite/__init__.py
@@ -1,3 +1,11 @@
 from .models import Remind, User, make_base
 from .base import TableNameMixin, TimestampMixin
 
+__all__ = [
+    "Remind",
+    "User",
+    "make_base",
+    "TableNameMixin",
+    "TimestampMixin",
+]
+
